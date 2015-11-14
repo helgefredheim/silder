6dc6d1438261ea8d0c42a7eac4a2e43e85d21b14
Helge Fredheim <helge.fredheim@bekk.no>
2015-11-14 11:50:07 +0100

    Laget eget grid - som forhåpentlig funker bedre på ipad

--- app/views/Product.jsx
+++ app/views/Product.jsx
@@ -25,11 +25,11 @@ var Product = React.createClass({
 		return <div className="page page--product solid">
 					<NavigationBar title="Silder" />
 					<section className="container section solid">
-						<div className="silder-row around">
-							<div cols="6" className="column">
+						<div className="product">
+							<div className="product-image">
 								<img alt={this.props.imageTitle} src={this.props.imageUrl + "?fit=thumb&w=600&h=600"} />
 							</div> 
-							<div cols="6" className="column">
+							<div className="product-details">
 								<div className="product-facts">
 									<h1 className="product-title">{this.props.productName}</h1>
 									<p aria-label={"kroner " + this.props.price} className="product-price"><span className="price-unit">kr</span> {this.props.price}</p>
--- app/views/ProductTeaser.jsx
+++ app/views/ProductTeaser.jsx
@@ -8,7 +8,7 @@ var ProductTeaser = React.createClass({
 	},
 
 	render: function() {
-		return <article cols="4" className={this.props.className}>
+		return <article className={this.props.className}>
 					<a href={this.getUrl()} title={"Mer informasjon om produktet " + this.props.productName}>
 						<div className="img-container">
 							<img alt={this.props.productName + ". Foto."} src={this.props.imageUrl + "?fit=thumb&w=600&h=600"} />
@@ -17,7 +17,6 @@ var ProductTeaser = React.createClass({
 						<h2>
 							{this.props.productName}
 						</h2>
-						<p>{this.props.introduction}</p>
 					</a>
 				</article>
 	}
