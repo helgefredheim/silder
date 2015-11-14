69be08e6e1b3661fbb15e22ac6a17f5f2c7403e6
Helge Fredheim <helge.fredheim@bekk.no>
2015-11-14 12:13:47 +0100

    Div. flikk for ipad

--- app/views/Products.jsx
+++ app/views/Products.jsx
@@ -3,19 +3,19 @@ var ProductTeaser = require("./ProductTeaser.jsx");
 
 var Products = React.createClass({
 	render: function() {
-		return <section className="section solid container">
-					<div className="solid">
+		return <section className="section solid">
+					<div className="container solid">
 						<h2 className="section-title">
 							<span>{this.props.title}</span>
 						</h2>
 					</div>
-					
+					<div className="container">
 						<div className="product-teasers">
 							{this.props.products.map(function (p) {
 								return <ProductTeaser key={p.slug} className="product-teaser" {...p} image="lampe.jpg" />;
 							})}
 						</div>
-					
+					</div>
 				</section>				
 	}
 });
